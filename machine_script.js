f998481e1885a843b5458c9af68ccf2fbdb276b5
Roland Arnoldt <rolandarnoldt@10-17-50-26.dynapool.wireless.nyu.edu>
2018-10-23 07:25:44 -0400

    added face recognition

--- machine_script.js
+++ machine_script.js
@@ -1,5 +1,6 @@
 //define globals
-var ident = "not identified"
+var ident = "not identified";
+var face_id = "not identified";
 
 window.addEventListener('load', function() {
     //define human_ident
@@ -25,7 +26,7 @@ window.addEventListener('load', function() {
             // minimum face size to expect, the higher the faster processing will be,
             // but smaller faces won't be detected
             minFaceSize: 20
-        }
+        };
 
         const results = await faceapi.mtcnn(document.getElementById('thevideo'), forwardParams);
 
@@ -38,7 +39,7 @@ window.addEventListener('load', function() {
                 if (faceDetection.score < minConfidence) {
                     console.log('not a face');
                     //return;
-                }
+                };
                 console.log('we got a face');
                 ident = "human elements detected";
                 console.log("face landmarks: ", faceLandmarks);
@@ -46,32 +47,37 @@ window.addEventListener('load', function() {
                 // faceapi.drawDetection('overlay', faceDetection);
                 // faceapi.drawLandmarks('overlay', faceLandmarks);
                 // return ident;
-            })
-        }
-    }
+            });
+        };
+    };
 
     //do a face check, connect to chat
     async function faceCheck() {
+        //detect face in webcam-stream
         await loadModels();
         await detectFaces();
         console.log("ident: ", ident)
         if (ident == "human elements detected") {
             connectToChat();
-        }
+        };
         if (ident == "not identified") {
             console.log("the thing:", ident)
             document.getElementById('human_ident').innerHTML = "<span style=\"color:black\">" + "updated scan status:"
             + "</span>" + "<span style=\"color:red\">" + ident + "</span>" + "<br /><br />";
+        };
+        // try to recognize face in webcam-stream
+        const descriptor1 = await faceapi.computeFaceDescriptor(document.getElementById('theface'))
+        const descriptor2 = await faceapi.computeFaceDescriptor(document.getElementById('thevideo'))
+        const distance = faceapi.euclideanDistance(descriptor1, descriptor2)
+        console.log(distance);
+        if (distance < 0.6) {
+            console.log('match');
+            face_id = "known";
         }
-        //const detections = await faceapi.tinyYolov2(document.getElementById('thevideo'), forwardParams)
-        // const descriptor1 = await faceapi.computeFaceDescriptor(document.getElementById('theface2'))
-        // const descriptor2 = await faceapi.computeFaceDescriptor(document.getElementById('theface'))
-        // const distance = faceapi.euclideanDistance(descriptor1, descriptor2)
-        // console.log(distance);
-        // if (distance < 0.6)
-        //   console.log('match')
-        // else
-        //   console.log('no match')
+        else {
+          console.log('no match');
+          face_id = "unknown";
+        };
     };
 
     // The video element on the page to display the webcam
@@ -80,7 +86,7 @@ window.addEventListener('load', function() {
     var storedFace = document.getElementById('theface');
     storedFace.src = 'roland1.png';
     var storedFace2 = document.getElementById('theface2');
-    storedFace2.src = 'roland5.png';
+    storedFace2.src = 'penny5.png';
     console.log("storedFaces are loaded");
     // Constraints - what do we want?
     let constraints = { audio: false, video: true };
@@ -136,7 +142,7 @@ window.addEventListener('load', function() {
 });
 
 var socket = io.connect();
-
+console.log("face_id top:", face_id);
 socket.on('connect', function() {
     console.log("Connected");
     if (ident) {document.getElementById('human_ident').innerHTML = "<span style=\"color:black\">" + "Scanning your visual interfaces"
@@ -170,15 +176,37 @@ socket.on('dataurl', function(data) {
     // display face-check status
 
     //get Base64 image data and user id of it
+    // function waitForFaceId() {
+    //     ident = "face checked already";
+    //     document.getElementById('human_ident').innerHTML = "updated scan status: human elements detected, connecting to human_machine chat";
+    //     //setTimeout(showTopIntro, 4000);
+    //     console.log("ident_low:", ident);
+    //     //console.log("face_id: ", face_id);
+    //     // if (face_id == "known"){
+    //     //document.getElementById('human_ident').innerHTML = "yes"
+    //         //"updated scan status: human elements detected, welcome back, Roland!"
+    //                 //+ "<br />" +  "connecting now to human_machine chat";
+    //     // }
+    //     // else {
+    //     //     document.getElementById('human_ident').innerHTML = "updated scan status: unknown human elements detected, connecting now to human_machine chat";
+    //     // };
+    //     // setTimeout(function(){showTopIntro()}, 4000);
+    //     // ident = "face_checked already";
+    // };
+
     if (ident == "human elements detected") {
         document.getElementById('human_ident').innerHTML = "updated scan status: human elements detected, connecting to human_machine chat";
-        setTimeout(function(){showTopIntro()}, 4000);
+        setTimeout(showTopIntro(), 4000);
         ident = "face_checked already"
     }
     if (ident == "face_checked already") {
         showTopIntro();
     }
+
     function showTopIntro(){
+        if (face_id == "known") {
+            document.getElementById('face_id').innerHTML = "Welcome back, Roland!";
+        };
         document.getElementById('human_ident').innerHTML = "In another reality I am half human, half machine."
             + "<br />" + "I can read Base64 and see you, "
             + "<span style=\"color:red\">" +
